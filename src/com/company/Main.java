b7c3067212fbcfe1eeb9c83b97d807af82d27ddd
Mefistosssss <oleksandrkondaurovit@gmail.com>
2022-01-29 08:04:14 -0800

    The basic structure is Complite

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         SolarSystem milkyway= new SolarSystem();
-        System.out.println("das");
+        milkyway.sun=1;
+        milkyway.moon=2;
+        milkyway.Planets=3;
+        System.out.println("");
     }
 }
